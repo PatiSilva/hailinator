1026607022d2fb07d34465b4cb080d227fd6317c
MakerSquare Student <karapendl@gmail.com>
2013-06-20 11:55:07 -0500

    Step 8 of 8.

--- hailinator.rb
+++ hailinator.rb
@@ -5,8 +5,19 @@ Twitter.configure do |config|
   config.oauth_token = "1498117129-DctkVKK1RKl9UaeZnBdDRA7DTyv4voVreoCyEx3"
   config.oauth_token_secret = "g5Shii7XmuEUl46OTry0Obrma2LX2gwEs10SeehGao"
 end
-Twitter.home_timeline
+# Twitter.home_timeline
 
-Twitter.search("hail", :result_type => "recent").results.map do |tweet|
-   puts "#{tweet.from_user}: #{tweet.text}"
+
+
+
+require "CSV"
+CSV.open("tweet.csv", "wb") do |csv| 
+
+  csv << ["handle", "text", "url"]
+  
+  Twitter.search("hail", :result_type => "recent").results.each do |tweet|
+   puts "#{tweet.from_user}: #{tweet.text}: #{tweet.url}"
+   puts tweet.inspect
+   end
+  csv << []
 end
