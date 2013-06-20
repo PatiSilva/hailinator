738d382f0eda07a54999fa728c447d7c6429b17e
MakerSquare Student <karapendl@gmail.com>
2013-06-20 09:45:20 -0500

    Initial commit.

--- hailinator.rb
+++ hailinator.rb
@@ -1,1 +1,8 @@
 require "twitter"
+Twitter.configure do |config|
+  config.consumer_key = "ce32LXzNTtCThAF6XrUWvA"
+  config.consumer_secret = "0NZGgfYEQXtMLTabikA3ydXHgCK1APmwjXvylEhOrbE"
+  config.oauth_token = "1498117129-DctkVKK1RKl9UaeZnBdDRA7DTyv4voVreoCyEx3"
+  config.oauth_token_secret = "g5Shii7XmuEUl46OTry0Obrma2LX2gwEs10SeehGao"
+end
+Twitter.home_timeline
